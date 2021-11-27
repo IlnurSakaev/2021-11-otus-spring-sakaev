914f3d9764ab4fc8f640ec34764896d731ea8b1b
Ilnur Sakaev <sakaev.ilnur.90@yandex.ru>
2021-11-27 14:17:40 +0300

    Optimize empty strings

--- 01-test-student/src/test/java/ru/isakaev/service/QuestionServiceImplTest.java
+++ 01-test-student/src/test/java/ru/isakaev/service/QuestionServiceImplTest.java
@@ -2,13 +2,10 @@ package ru.isakaev.service;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import ru.isakaev.dao.QuestionDao;
 
 
 import java.net.URISyntaxException;
-
-import static org.mockito.BDDMockito.given;
 
 class QuestionServiceImplTest {
 
