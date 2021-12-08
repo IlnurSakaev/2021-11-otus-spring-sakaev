5ef30b0d9a455b939f162777a0127595828b4f04
isakaev <isakaev@akber-soft.ru>
2021-12-08 10:35:15 +0500

    correct main

---
+++ 02-test-student/src/test/java/ru/isakaev/service/StudentServiceImplTest.java
@@ -0,0 +1,35 @@
+
+package ru.isakaev.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.isakaev.model.Student;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
+class StudentServiceImplTest {
+
+    private StudentService studentService;
+
+    @Test
+    void getStudent() throws NoSuchFieldException, IllegalAccessException {
+        studentService = new StudentServiceImpl();
+        Student testStudent = new Student("Ilnur", "Sakaev", 3);
+        InputStream stdin = System.in;
+
+        //        scanner
+        Field scanner = studentService.getClass().getDeclaredField("scanner");
+        scanner.setAccessible(true);
+        scanner.set(studentService, new Scanner(new ByteArrayInputStream("Ilnur\nSakaev".getBytes())));
+
+        try {
+            Student student = studentService.getStudent();
+            Assertions.assertThat(student.equals(testStudent));
+        } finally {
+            System.setIn(stdin);
+        }
+
+    }
+}
---
+++ 02-test-student/src/test/java/ru/isakaev/service/TestServiceImplTest.java
@@ -0,0 +1,54 @@
+package ru.isakaev.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;;
+import ru.isakaev.model.Question;
+import ru.isakaev.model.Student;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+
+class TestServiceImplTest {
+
+    @Test
+    void testStudent() throws IllegalAccessException, NoSuchFieldException {
+
+        Student student = new Student("Ilnur", "Sakaev", 3);
+        Set<Question> questionSet = Set.of(
+                new Question("One plus one",
+                        new String[]{"one","two","three"}, "two"),
+                new Question("One plus two",
+                        new String[]{"one","two","three"}, "three")
+        );
+
+        QuestionService questionService = Mockito.mock(QuestionService.class);
+        when(questionService.loadQuestions()).thenReturn(questionSet);
+
+        StudentService studentService = Mockito.mock(StudentService.class);
+        when(studentService.getStudent()).thenReturn(student);
+
+
+        TestServiceImpl test = new TestServiceImpl(studentService, questionService);
+
+        InputStream stdin = System.in;
+
+        Field passingBarrier = test.getClass().getDeclaredField("passingBarrier");
+        passingBarrier.setAccessible(true);
+        passingBarrier.set(test, 1);
+
+        Field scanner = test.getClass().getDeclaredField("scanner");
+        scanner.setAccessible(true);
+        scanner.set(test, new Scanner(new ByteArrayInputStream("three\ntwo\nexit".getBytes())));
+
+        try {
+            test.testStudent();
+        } finally {
+            System.setIn(stdin);
+        }
+    }
+}
